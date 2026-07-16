1f81b71d2a130f62d4865190689a45a888a13c71
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-07-16 18:13:30 -0400

    Just use a bunch of bools organized under feature tabs
    Probably should just use TabWidget in the editor.

--- Editor/ShaderGraphPlus/ShaderTemplate/ShaderTemplateResource.cs
+++ Editor/ShaderGraphPlus/ShaderTemplate/ShaderTemplateResource.cs
@@ -4,31 +4,50 @@ using static Editor.Inspectors.AssetInspector;
 
 namespace ShaderGraphPlus;
 
-
 [AssetType( Name = "Shader Template", Extension = "shdrtpl" )]
 public sealed class ShaderTemplateResource
 {
-	public Dictionary<string, bool> Features { get; set; } = new()
-	{
-		{ "SupportsAlbedo", true},
-		{ "SupportsEmission", true },
-		{ "SupportsOpacity", true },
-		{ "SupportsNormal", true },
-		{ "SupportsRoughness", true },
-		{ "SupportsMetalness", true },
-		{ "SupportsAmbientOcclusion", true },
-		{ "SupportsPositionOffset", true },
-		{ "SupportsPixelDepthOffset", true },
+	[Feature( "Supported Material Inputs", Icon = "input" )]
+	public bool Albedo { get; set; } = true;
 
-		{ "SupportsLitShadingModel", true },
-		{ "SupportsUnlitShadingModel", true },
-		{ "SupportsCustomShadingModel", true },
+	[Feature( "Supported Material Inputs", Icon = "input" ), ShowIf( nameof( LitShading ), true )]
+	public bool Emission { get; set; } = true;
 
-		{ "SupportsOpaqueBlendMode", true },
-		{ "SupportsMaskedBlendMode", true },
-		{ "SupportsTranslucentBlendMode", true },
-		{ "SupportsDynamicBlendMode", true }
-	};
+	[Feature( "Supported Material Inputs", Icon = "input" )]
+	public bool Opacity { get; set; } = true;
+
+	[Feature( "Supported Material Inputs", Icon = "input" ), ShowIf( nameof( LitShading ), true )]
+	public bool Normal { get; set; } = true;
+
+	[Feature( "Supported Material Inputs", Icon = "input" ), ShowIf( nameof( LitShading ), true )]
+	public bool Roughness { get; set; } = true;
+
+	[Feature( "Supported Material Inputs", Icon = "input" ), ShowIf( nameof( LitShading ), true )]
+	public bool Metalness { get; set; } = true;
+
+	[Feature( "Supported Material Inputs", Icon = "input" ), ShowIf( nameof( LitShading ), true )]
+	public bool AmbientOcclusion { get; set; } = true;
+
+	[Feature( "Supported Material Inputs", Icon = "input" )]
+	public bool PositionOffset { get; set; } = true;
+
+	[Feature( "Supported Shading Models", Icon = "tonality" )]
+	public bool LitShading { get; set; } = true;
+
+	[Feature( "Supported Shading Models", Icon = "tonality" )]
+	public bool UnlitShading { get; set; } = true;
+
+	[Feature( "Supported Blend Modes", Icon = "tonality" )]
+	public bool OpaqueBlend { get; set; } = true;
+
+	[Feature( "Supported Blend Modes", Icon = "tonality" )]
+	public bool MaskedBlend { get; set; } = true;
+
+	[Feature( "Supported Blend Modes", Icon = "tonality" )]
+	public bool TranslucentBlend { get; set; } = true;
+
+	[Feature( "Supported Blend Modes", Icon = "tonality" )]
+	public bool DynamicBlend { get; set; } = true;
 
 	[TextArea]
 	public string Code { get; set; } = "";
