41e5f420604f13bd464ff362fcacede9569107fd
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-07-21 16:05:51 -0400

    Default code

--- Editor/ShaderGraphPlus/ShaderTemplate/ShaderTemplateResource.cs
+++ Editor/ShaderGraphPlus/ShaderTemplate/ShaderTemplateResource.cs
@@ -53,5 +53,65 @@ public sealed partial class ShaderTemplateResource
 	public bool DynamicBlend { get; set; } = true;
 
 	[TabPage( "Code" ), TextArea]
-	public string Code { get; set; } = "";
+	public string Code { get; set; } = DefaultCode;
+
+	[Hide]
+	public static string DefaultCode => @"
+HEADER
+{{
+	Description = ""{sgp_shader_description}"";
+}}	
+
+FEATURES
+{{
+{sgp_shader_feature_defines}
+}}
+
+MODES
+{{
+	Forward();
+	Depth();
+	ToolsShadingComplexity( ""tools_shading_complexity.shader"" );
+}}
+
+COMMON
+{{
+{sgp_shader_common}
+}}
+
+struct VertexInput
+{{
+	#include ""common/vertexinput.hlsl""
+{sgp_vertex_input_data}
+}};
+
+struct PixelInput
+{{
+	#include ""common/pixelinput.hlsl""
+{sgp_pixel_input_data}
+}};
+
+VS
+{{
+	#include ""common/vertex.hlsl""
+{sgp_vertex_globals}{sgp_vertex_combo_rules}{sgp_vertex_functions}
+	PixelInput MainVs( VertexInput v )
+	{{
+{sgp_vertex_code}
+	}}
+}}
+
+PS
+{{
+	#include ""common/pixel.hlsl""
+{sgp_pixel_globals}{sgp_pixel_combo_rules}{sgp_pixel_functions}
+	float4 MainPs( PixelInput i ) : SV_Target0
+	{{
+{sgp_pixel_init}
+{sgp_pixel_locals}
+{sgp_pixel_material}
+{sgp_pixel_output}
+	}}
+}}
+	";
 }
