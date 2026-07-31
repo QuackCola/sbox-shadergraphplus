59a244dd11fc6da6e103708ce50f244854eab7ba
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-07-31 16:01:20 -0400

    Add  ProjectionToView node

---
+++ Editor/ShaderGraphPlus/Nodes/ProjectionToViewNode.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ShaderGraphPlus.Nodes;
+
+/// <summary>
+/// Projection-space coordinates converted to view space
+/// </summary>
+[Title( "Projection To View" ), Category( "Variables/Matrix" ), Icon( "apps" )]
+public sealed class ProjectionToViewNode : ShaderNodePlus
+{
+	[JsonIgnore, Hide, Browsable( false )]
+	public override Color NodeTitleColor => ShaderGraphPlusTheme.NodeHeaderColors.GlobalVariableNode;
+
+	[JsonIgnore, Hide, Browsable( false )]
+	public override bool CanPreview => false;
+
+	[Output( typeof( Float4x4 ) ), Title( "Matrix" )]
+	[Hide]
+	public static NodeResult.Func Result => ( GraphCompiler compiler ) => new NodeResult( ResultType.Float4x4, "g_matProjectionToView" );
+}
