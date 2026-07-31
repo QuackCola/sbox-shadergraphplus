43b1d60de2859eca7b5576d5a3f0c17e23095bcb
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-07-31 15:24:08 -0400

    Add RandomFloats node

---
+++ Editor/ShaderGraphPlus/Nodes/RandomFloats.cs
@@ -0,0 +1,15 @@
+﻿namespace ShaderGraphPlus.Nodes;
+
+/// <summary>
+/// Random floats in each component, values change every frame
+/// </summary>
+[Title( "Random Floats" ), Category( "Variables" ), Icon( "casino" )]
+public sealed class RandomFloats : ShaderNodePlus
+{
+	[JsonIgnore, Hide, Browsable( false )]
+	public override Color NodeTitleColor => ShaderGraphPlusTheme.NodeHeaderColors.GlobalVariableNode;
+
+	[Output( typeof( Vector4 ) ), Title( "XYZW" )]
+	[Hide]
+	public static NodeResult.Func Result => ( GraphCompiler compiler ) => new NodeResult( ResultType.Vector4, "g_vRandomFloats.xyzw" );
+}
