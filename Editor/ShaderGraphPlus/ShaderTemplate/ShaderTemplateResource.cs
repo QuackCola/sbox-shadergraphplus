3381610e9fbfbe097b8c78509e616bad946b5525
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-08-04 07:08:04 -0400

    Fix default template code
    Forgor to update it 💀

--- Editor/ShaderGraphPlus/ShaderTemplate/ShaderTemplateResource.cs
+++ Editor/ShaderGraphPlus/ShaderTemplate/ShaderTemplateResource.cs
@@ -80,35 +80,61 @@ COMMON
 struct VertexInput
 {{
 	#include ""common/vertexinput.hlsl""
+	float4 vColor : COLOR0 < Semantic( Color ); >;
 {sgp_vertex_input_data}
 }};
 
 struct PixelInput
 {{
 	#include ""common/pixelinput.hlsl""
+	float3 vPositionOs : TEXCOORD14;
+	float3 vNormalOs : TEXCOORD15;
+	float4 vTangentUOs_flTangentVSign : TANGENT	< Semantic( TangentU_SignV ); >;
+	float4 vColor : COLOR0;
+	float4 vTintColor : COLOR1;
+	#if ( PROGRAM == VFX_PROGRAM_PS )
+		bool vFrontFacing : SV_IsFrontFace;
+	#endif
 {sgp_pixel_input_data}
 }};
 
 VS
 {{
 	#include ""common/vertex.hlsl""
-{sgp_vertex_globals}{sgp_vertex_combo_rules}{sgp_vertex_functions}
+
+{sgp_vertex_globals}
+{sgp_vertex_combo_rules}
+{sgp_vertex_functions}
+
 	PixelInput MainVs( VertexInput v )
 	{{
+		PixelInput i = ProcessVertex( v );
+		i.vPositionOs = v.vPositionOs.xyz;
+		i.vColor = v.vColor;
+
+		ExtraShaderData_t extraShaderData = GetExtraPerInstanceShaderData( v.nInstanceTransformID );
+		i.vTintColor = extraShaderData.vTint;
+
+		VS_DecodeObjectSpaceNormalAndTangent( v, i.vNormalOs, i.vTangentUOs_flTangentVSign );
 {sgp_vertex_code}
+		return FinalizeVertex( i );
 	}}
 }}
 
 PS
 {{
 	#include ""common/pixel.hlsl""
-{sgp_pixel_globals}{sgp_pixel_combo_rules}{sgp_pixel_functions}
+
+{sgp_pixel_globals}
+{sgp_pixel_combo_rules}
+{sgp_pixel_functions}
+
 	float4 MainPs( PixelInput i ) : SV_Target0
 	{{
-{sgp_pixel_init}
-{sgp_pixel_locals}
-{sgp_pixel_material}
-{sgp_pixel_output}
+
+{sgp_pixel_code}
+
+		return {sgp_pixel_output};
 	}}
 }}
 	";
