6012a948f3c9c8d17c916c1980e2870dcffef9c2
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-09-06 05:23:07 -0400

    Start working on preventing the user being able to create parameters with reserved names

---
+++ Editor/ShaderGraphPlus/Compiler/GraphCompiler.Reserved.cs
@@ -0,0 +1,66 @@
+﻿namespace ShaderGraphPlus;
+
+// Reserved names for things so we can show an error to the user
+// when they try to name a parameter, Feature or Combo to any name
+// below.
+
+// TODO : Hookup this stuff to the Blackboard, GraphCompiler and the
+// ShaderGraphPlus editor window.
+
+public sealed partial class GraphCompiler
+{
+	public static List<string> ReservedGlobalParameters => new()
+	{
+		// Floats
+		"g_flPreviewTime",
+		"g_flTime",
+
+		// Vectors
+		"g_vFrameBufferCopyInvSizeAndUvScale",
+		"g_vInvProjRow3",
+		"g_vRandomFloats",
+		"g_vRenderTargetSize",
+
+		"g_vViewportSize",
+		"g_vInvViewportSize",
+		"g_vViewportOffset",
+		"g_flViewportMinZ",
+		"g_flViewportMaxZ",
+
+		"g_vCameraPositionWs",
+		"g_vCameraDirWs",
+		"g_vCameraUpDirWs",
+		"g_flCameraFOV",
+		"g_flNearPlane",
+		"g_flFarPlane",
+
+		// Matrices
+		"g_matViewToProjection",
+		"g_matWorldToProjection",
+		"g_matProjectionToView",
+		"g_matProjectionToWorld",
+		"g_matWorldToView",
+
+		// Textures
+		"g_tFrameBufferCopyTexture",
+		"g_tColorBuffer",
+
+		// Attributes
+		"bWantsFBCopyTexture",
+	};
+
+	public static List<string> ReservedFeatureNames => new()
+	{
+
+	};
+
+	public static List<string> ReservedComboNames => new()
+	{
+
+	};
+
+	public static List<string> ReservedFunctionNames => new()
+	{
+
+	};
+}
