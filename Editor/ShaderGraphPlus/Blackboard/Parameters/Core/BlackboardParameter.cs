937ab30d32ee6dc00bbd903aed1ee1cbb804947d
QuackCola <23345567+QuackCola@users.noreply.github.com>
2026-09-24 16:43:14 -0400

    Remove this

--- Editor/ShaderGraphPlus/Blackboard/Parameters/Core/BlackboardParameter.cs
+++ Editor/ShaderGraphPlus/Blackboard/Parameters/Core/BlackboardParameter.cs
@@ -64,7 +64,7 @@ public interface IBlackboardSubgraphOutputParameter : IBlackboardSubgraphParamet
 
 public abstract class BlackboardParameter : IGroupableBlackboardParameter, IValid
 {
-	[Hide, Browsable( false ), Order( 0 )]
+	[Hide, Browsable( false )]
 	public Guid Identifier { get; set; }
 
 	[Browsable( false )]
